24c19a6d3ab1c383fd40d570df533a2d42f6fa7e
concept <conceptcodes@gmail.com>
2024-01-26 00:37:15 -0500

    Refactor database models, records associated to a tenant will delete when a tenant is deleted

--- pkg/models/db_model.go
+++ pkg/models/db_model.go
@@ -1,23 +1,23 @@
 package models
 
 import (
-	"time"
+    "time"
 
-	"gorm.io/gorm"
+    "gorm.io/gorm"
 )
 
 type RecordModel struct {
-	gorm.Model
-	ID        string    `gorm:"primaryKey;type:text;unique_index"`
-	TenantId  string    `gorm:"primaryKey;type:varchar(36);foreignkey:TenantRefer"`
-	Value     string    `gorm:"type:text"`
-	ExpiresAt time.Time `gorm:"type:timestamp"`
-	Tenant    TenantModel
+    gorm.Model
+    ID        string    `gorm:"primaryKey;type:text;unique_index"`
+    TenantId  string    `gorm:"primaryKey;type:varchar(36);"`
+    Value     string    `gorm:"type:text"`
+    ExpiresAt time.Time `gorm:"type:timestamp"`
+    Tenant    TenantModel `gorm:"foreignKey:TenantId;constraint:OnDelete:CASCADE;"`
 }
 
 type TenantModel struct {
-	gorm.Model
-	ID     string `gorm:"primaryKey;type:varchar(36);unique_index"`
-	Name   string `gorm:"type:varchar(100);unique_index"`
-	Secret string `gorm:"type:varchar(36)"`
+    gorm.Model
+    ID     string `gorm:"primaryKey;type:varchar(36);unique_index"`
+    Name   string `gorm:"type:varchar(100);unique_index"`
+    Secret string `gorm:"type:varchar(36)"`
 }
