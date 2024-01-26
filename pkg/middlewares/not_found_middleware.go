6694d7c52b4f94af7b3fffb821a6ce7db40b6fd0
concept <conceptcodes@gmail.com>
2024-01-26 00:26:19 -0500

    Refactor not found middleware to always send error response

--- pkg/middlewares/not_found_middleware.go
+++ pkg/middlewares/not_found_middleware.go
@@ -8,10 +8,7 @@ import (
 
 func NotFound(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		next.ServeHTTP(w, r)
-
-		if w.Header().Get("X-Content-Type-Options") == "" {
-			helpers.SendErrorResponse(w, "Not Found", constants.NotFound)
-		}
+		helpers.SendErrorResponse(w, "Not Found", constants.NotFound)
+		return
 	})
 }
